8e411a7d396da9791b3d9db7972be441c655f041
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-04 00:32:57 +0900

    お気に入りのpictureの一覧画面を、新しいルーティングのページで作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,13 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorite = Favorite.all
+  end
   def create
-    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    @favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_path
   end
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_path
   end
 end
